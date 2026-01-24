0bfa83a903987d56b855f58ce1bbf63f5e3af75d
danysis.2022@gmail.com <danysis.2022@gmail.com>
2026-01-24 16:59:50 -0500

    analytics puestas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 import "./App.css";
 
 import Header from "./components/header/header";
@@ -25,7 +26,9 @@ function App() {
       <Footer />
 
       <WhatsappIcon className="whatsapp-Icon" size={55} />
+      <Analytics />
     </BrowserRouter>
+
   );
 }
 
